3ce91f3a3940449aa2b989cf56a02e0928da5076
Kuldip Patel <patel.kuldip91@gmail.com>
2022-07-16 17:42:36 -0700

    Add 1013-fibonacci-number

---
+++ problems/1013-fibonacci-number/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int n) {
+        if(n < 1)
+            return 0;
+        else if(n < 2)
+            return 1;
+        
+        return fib(n - 1) + fib(n - 2);
+    }
+}
