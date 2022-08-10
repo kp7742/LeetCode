17888395c1294b392f4661bfa53d631d3d681e7f
Kuldip Patel <patel.kuldip91@gmail.com>
2022-08-10 01:06:55 -0700

    Add 852-peak-index-in-a-mountain-array

---
+++ problems/852-peak-index-in-a-mountain-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0, r = arr.length - 1;
+        while(l < r){
+            int m = l + (r - l) / 2;
+            
+            if(arr[m] < arr[m + 1]){
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return l;
+    }
+}
